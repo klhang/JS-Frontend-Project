d72301fd2a0df2a564336d9e72a75111c76fc080
Klhang <chrisjunhang@gmail.com>
2017-10-06 09:42:43 -0700

    delete image folder

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -75,7 +75,7 @@ function bubbleChart() {
   // @v4 scales now have a flattened naming scheme
   var fillColor = d3.scaleOrdinal()
     .domain(['down', 'flat', 'up'])
-    .range(['#d84b2a', '#beccae', '#7aa25c']);
+    .range(['Tomato', 'Orange', 'MediumSeaGreen']);
 
 
   /*
